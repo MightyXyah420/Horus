fa34d8e0aa7738513b831341f0a3a9026c6fdf70
MightyXyah420 <juancnq16@gmail.com>
2021-10-06 18:31:20 -0500

    spring boot admin added

--- src/main/java/com/gd/horus/HorusApplication.java
+++ src/main/java/com/gd/horus/HorusApplication.java
@@ -11,7 +11,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+ 
+
 @SpringBootApplication
+@EnableAdminServer
 public class HorusApplication {
 
 	public static void main(String[] args) {
